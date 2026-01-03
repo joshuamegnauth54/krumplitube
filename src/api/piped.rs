575f50522fd48fd5cae40485205c610893d6fe93
Josh Megnauth <jo.sh@tutanota.com>
2026-01-02 22:22:06 -0500

    piped: Types for /streams:video_id
    Some of the structs contain repeated fields that can be refactored into
    their own types which are then flattened into the parents.
    
    I'm also not sure what I can skip - it seems like a lot of the fields
    aren't useful for this program.

--- src/api/piped.rs
+++ src/api/piped.rs
@@ -9,17 +9,38 @@ use serde::Deserialize;
 use smol_str::SmolStr;
 use url::Url;
 
+/// /streams:video_id
 #[derive(Deserialize)]
 #[serde(rename = "camelCase")]
 pub struct Streams {
     pub audio_streams: Vec<AudioStream>,
     pub index_start: u8,
+    pub dash: Option<Url>,
+    pub description: String,
+    pub dislikes: u32,
+    pub likes: u32,
+    pub duration: u32,
+    pub hls: Option<Url>,
+    #[serde(skip)]
+    pub lbry_id: Option<Url>,
+    pub livestream: bool,
+    pub proxy_url: Url,
+    pub related_streams: Vec<RelatedStream>,
+    pub subtitles: Vec<Subtitles>,
+    pub thumbnail_url: Url,
+    pub title: String,
+    pub upload_date: SmolStr,
+    pub uploader: SmolStr,
+    pub uploader_url: Url,
+    pub uploader_verified: bool,
+    pub video_streams: Vec<VideoStream>,
+    pub views: u32,
 }
 
 #[derive(Deserialize)]
 #[serde(rename = "camelCase")]
 pub struct AudioStream {
-    pub bitrate: u16,
+    pub bitrate: u32,
     pub codec: SmolStr,
     pub format: SmolStr,
     pub index_end: u32,
@@ -30,4 +51,47 @@ pub struct AudioStream {
     pub mime_type: Mime,
     pub url: Url,
     pub video_only: bool,
+}
+
+#[derive(Deserialize)]
+#[serde(rename = "camelCase")]
+pub struct RelatedStream {
+    pub duration: u32,
+    pub thumbnail: Url,
+    pub title: String,
+    pub uploaded_date: SmolStr,
+    pub uploaded_avatar: Url,
+    pub uploader_url: Url,
+    pub uploader_verified: bool,
+    pub url: Url,
+    pub views: u32,
+}
+
+#[derive(Deserialize)]
+#[serde(rename = "camelCase")]
+pub struct Subtitles {
+    pub auto_generated: bool,
+    pub code: SmolStr,
+    #[serde(with = "crate::api::mime")]
+    pub mime_type: Mime,
+    pub url: Url,
+}
+
+#[derive(Deserialize)]
+#[serde(rename = "camelCase")]
+pub struct VideoStream {
+    pub bitrate: u32,
+    pub codec: SmolStr,
+    pub format: SmolStr,
+    pub fps: u16,
+    pub height: u16,
+    pub width: u16,
+    pub index_end: u32,
+    pub index_start: u32,
+    pub init_end: u32,
+    pub init_start: u32,
+    #[serde(with = "crate::api::mime")]
+    pub mime_type: Mime,
+    pub url: Url,
+    pub quality: SmolStr,
 }
